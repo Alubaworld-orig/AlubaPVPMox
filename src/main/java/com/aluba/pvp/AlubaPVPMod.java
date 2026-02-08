308c5343d878cd5dd69bd7a02a0c5d58ce3fdfe8
Alubaworld-orig <aluba2614@gmail.com>
2026-02-08 15:58:13 +0300

    Update AlubaPVPMod.java

--- src/main/java/com/aluba/pvp/AlubaPVPMod.java
+++ src/main/java/com/aluba/pvp/AlubaPVPMod.java
@@ -7,26 +7,33 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// Название мода должно совпадать с тем, что в mods.toml
 @Mod("alubapvpmod")
 public class AlubaPVPMod {
     public static final String MOD_ID = "alubapvpmod";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public AlubaPVPMod() {
-        // Регистрируем наш мод в системе Forge
+        // Регистрация события загрузки клиентской части
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
         
-        // Эта строчка заставляет работать все наши будущие функции
+        // Регистрация главного класса в шине событий Forge
         MinecraftForge.EVENT_BUS.register(this);
         
-        LOGGER.info("AlubaPVPMod: Система активирована!");
+        LOGGER.info("AlubaPVPMod: Инициализация началась...");
     }
 
     private void clientSetup(final FMLClientSetupEvent event) {
-        // Вот эти две строчки активируют твои функции!
+        // ВКЛЮЧАЕМ ВСЕ МОДУЛИ:
+        
+        // 1. Красное кольцо и сглаживание
         MinecraftForge.EVENT_BUS.register(new PvpGraphics());
+        
+        // 2. Статистика врага (HP и Броня)
         MinecraftForge.EVENT_BUS.register(new EnemyStats());
         
-        LOGGER.info("AlubaPVPMod: Все функции успешно запущены!");
+        // 3. Плавная анимация чанков
+        MinecraftForge.EVENT_BUS.register(new ChunkAnimator());
+
+        LOGGER.info("AlubaPVPMod: Все PVP системы (Кольцо, Статы, Чанки) запущены!");
     }
+}
